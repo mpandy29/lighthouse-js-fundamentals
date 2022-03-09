d27b8c76d191aa6214c99711926308dbc5f060a5
Michael Francis Pandy <michaelpandy29@gmail.com>
2022-03-09 04:02:40 +0000

    Simple functions examples

---
+++ hello.js
@@ -0,0 +1,13 @@
+// example number 1 = simple function
+
+/*const sayHello = function(){
+  console.log("Hello, world");
+}
+sayHello();
+sayHello();*/
+
+const sayHello = function(name){
+  console.log("Hello, " + name );
+}
+
+sayHello("Mike");
